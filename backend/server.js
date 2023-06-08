5df0535e20c89c57cd339456f2b9829e74c01718
Noam Rivlin <noam.rivlin15@gmail.com>
2023-06-08 13:48:43 +0300

    WIP - implementing redux

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,14 @@
-import { express } from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
+const express = require('express');
+const dotenv = require('dotenv')
+dotenv.config({ path: `${__dirname}/.env` });
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const PORT = process.env.PORT || 4000
 
-// app.use('/api/', displayImages);
-
-
+// app.use('/api/gallery', displayGallery);
 
 // app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 function App() {
+  console.log('client is running')
   return (
     <div className="App">
 
