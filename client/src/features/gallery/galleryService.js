f6be837a703a6ea118392b32e31d81255356c369
Noam Rivlin <noam.rivlin15@gmail.com>
2023-06-17 00:28:51 +0300

    changes to deploy- start script and changing api endpoint to be from env

--- client/src/features/gallery/galleryService.js
+++ client/src/features/gallery/galleryService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // the API_URL is the URL of the server that we will use to fetch the images
-const API_URL = 'http://localhost:5000/api/gallery/';
+// const API_URL = 'http://localhost:5000/api/gallery/';
+const API_URL = process.env.REACT_APP_API_URL
 
 // fetchImages is a function that fetches images from the API
 // it takes the page number and category as params
