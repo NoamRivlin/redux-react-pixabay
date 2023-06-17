859beb5445be737601851ba2c8cd8c9fab322e27
Noam Rivlin <88899637+NoamRivlin@users.noreply.github.com>
2023-06-17 14:23:03 +0300

    Update galleryService.js
    troubleshooting

--- client/src/features/gallery/galleryService.js
+++ client/src/features/gallery/galleryService.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 // the API_URL is the URL of the server that we will use to fetch the images
 const API_URL = process.env.REACT_APP_API_URL ;
+console.log(API_URL);
 
 // fetchImages is a function that fetches images from the API
 // it takes the page number and category as params
