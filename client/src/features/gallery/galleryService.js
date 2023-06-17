5fdf1aa6e47e0fa75c8fee5b39553426263e4930
Noam Rivlin <noam.rivlin15@gmail.com>
2023-06-17 14:30:06 +0300

    WIP- in backend enabling cors to not just http://localhost:3000

--- client/src/features/gallery/galleryService.js
+++ client/src/features/gallery/galleryService.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // the API_URL is the URL of the server that we will use to fetch the images
-const API_URL = process.env.REACT_APP_API_URL + '/api/gallery';
-
+const API_URL = process.env.REACT_APP_API_URL;
+console.log(API_URL);
 // fetchImages is a function that fetches images from the API
 // it takes the page number and category as params
 // it returns the response from the API
